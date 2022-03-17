1fadc6a7c2e1bce91449f122a25f9fb8940b0ace
Niansong Zhang <zhangns@mail2.sysu.edu.cn>
2022-03-16 20:09:29 -0400

    add lit test

---
+++ tests/test.c
@@ -0,0 +1,16 @@
+// RUN: clang %s
+int main() {
+    int a = 1;
+    int b = 1;
+    int c = a + b;
+    int d = a + b;
+    int e = d;
+    if (c > 1) {
+      a = 1;
+      b = 2;
+    } else {
+      a = 2;
+      b = 1;
+    }
+    a += 1;
+}
